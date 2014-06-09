0c357099a0ad663b1fe902ebb55d91e6e1c6dfe1
Bruno Filippone <bruno.filippone@ovoenergy.com>
2014-06-09 17:40:47 +0100

    Updated FutureChainableActions

--- src/main/scala/com/bfil/scalext/actions/FutureChainableActions.scala
+++ src/main/scala/com/bfil/scalext/actions/FutureChainableActions.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success}
 import shapeless.{:: => ::, HNil}
 
 trait FutureChainableActions[T] extends BasicChainableActions[T] {
-  def resolveFuture[T](f: => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
+  def onComplete[T](f: => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
     def happly(inner: (T :: HNil) => Action) = { ctx =>
       f.onComplete {
         case Success(t)  => inner(t :: HNil)(ctx)
@@ -15,11 +15,27 @@ trait FutureChainableActions[T] extends BasicChainableActions[T] {
     }
   }
 
-  def resolveFuture[T](f: Context => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
+  def onComplete[T](f: Context => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
     def happly(inner: (T :: HNil) => Action) = { ctx =>
       f(ctx).onComplete {
         case Success(t)  => inner(t :: HNil)(ctx)
         case Failure(ex) => throw ex
+      }
+    }
+  }
+  
+  def onSuccess[T](f: => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
+    def happly(inner: (T :: HNil) => Action) = { ctx =>
+      f.onSuccess {
+        case t  => inner(t :: HNil)(ctx)
+      }
+    }
+  }
+  
+  def onSuccess[T](f: Context => Future[T])(implicit ec: ExecutionContext): ChainableAction1[T] = new ChainableAction1[T] {
+    def happly(inner: (T :: HNil) => Action) = { ctx =>
+      f(ctx).onSuccess {
+        case t  => inner(t :: HNil)(ctx)
       }
     }
   }
