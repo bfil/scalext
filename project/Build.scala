62b2c2bcd28013c9368516b5e471737ccfd284c0
Bruno Filippone <bruno.filippone@b-fil.com>
2014-11-02 18:59:43 +0000

    Updated build to use the new Nexus repo and updated the README

--- project/Build.scala
+++ project/Build.scala
@@ -1,76 +1,23 @@
 import sbt._
 import Keys._
+import com.bfil.sbt._
 
-object ScalextBuild extends Build {
+object ProjectBuild extends BFilBuild with BFilPlugins {
 
   val buildVersion = "0.2.0-SNAPSHOT"
-
-  lazy val Scalext = Project(
-    id = "scalext",
-    base = file("."),
-    settings = Defaults.defaultSettings ++
-      buildSettings ++
-      compilersSettings ++
-      Publish.settings)
-
-  lazy val buildSettings = Seq(
-    name := "scalext",
-    organization := "com.bfil",
-    version := buildVersion,
-    scalaVersion := "2.11.1",
-    crossScalaVersions  := Seq("2.11.1", "2.10.4"),
-    crossVersion := CrossVersion.binary,
-    organizationName := "Bruno Filippone",
-    organizationHomepage := Some(url("http://www.b-fil.com")),
-    libraryDependencies <++= scalaVersion(Dependencies.all(_)),
-    resolvers ++= Resolvers.all)
-
-  lazy val compilersSettings = Seq(
-    scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
-    javacOptions ++= Seq("-Xlint:unchecked", "-Xlint:deprecation"))
+    
+  lazy val project = BFilProject("scalext", file("."))
+  .settings(libraryDependencies ++= Dependencies.all(scalaVersion.value))
 }
 
 object Dependencies {
   def all(scalaVersion: String) = Seq(
     scalaVersion match {
-      case "2.11.1" => "com.chuusai" %% "shapeless" % "2.0.0"
+      case "2.11.2" => "com.chuusai" %% "shapeless" % "2.0.0"
       case "2.10.4" => "com.chuusai" %% "shapeless" % "1.2.4"
     },
     "com.typesafe.akka" %% "akka-actor" % "2.3.3",
     "org.specs2" %% "specs2" % "2.3.12" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test",
     "org.hamcrest" % "hamcrest-all" % "1.3" % "test")
-}
-
-object Resolvers {
-  val all = Seq(
-    "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/")
-}
-
-object Publish {
-  def repository: Project.Initialize[Option[sbt.Resolver]] = version { (version: String) =>
-    val s3Bucket = "s3://bfil-mvn-repo.s3-eu-west-1.amazonaws.com/"
-    if (version.trim.endsWith("SNAPSHOT")) Some("BFil S3 Repo Snapshots" at s3Bucket + "snapshots")
-    else Some("BFil S3 Repo Releases" at s3Bucket + "releases")
-  }
-
-  lazy val settings = Seq(
-    publishMavenStyle := true,
-    publishTo <<= repository,
-    publishArtifact in Test := false,
-    pomIncludeRepository := { _ => false },
-    licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-    homepage := Some(url("http://www.b-fil.com")),
-    pomExtra := (
-      <scm>
-        <url>git://github.com/bfil/scalext.git</url>
-        <connection>scm:git://github.com/bfil/scalext.git</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>bfil</id>
-          <name>Bruno Filippone</name>
-          <url>http://www.b-fil.com</url>
-        </developer>
-      </developers>))
 }
