26a0ccb3fa1934214f4983e0f3b24b237246529c
Bruno Filippone <bruno.filippone@b-fil.com>
2015-03-21 18:17:16 +0000

    Split testkit and tests into separate modules

--- project/Build.scala
+++ project/Build.scala
@@ -2,19 +2,35 @@ import sbt._
 import Keys._
 import com.bfil.sbt._
 
-object ProjectBuild extends BFilBuild with BFilPlugins {
+object ProjectBuild extends BFilBuild {
 
   val buildVersion = "0.2.0-SNAPSHOT"
-    
-  lazy val project = BFilProject("scalext", file("."))
-  .settings(libraryDependencies ++= Dependencies.all(scalaVersion.value))
+
+  lazy val root = BFilRootProject("root", file("."))
+    .aggregate(scalext, scalextTestkit, scalextTests)
+
+  lazy val scalext = BFilProject("scalext", file("scalext"))
+    .settings(libraryDependencies ++= Dependencies.core(scalaVersion.value))
+
+  lazy val scalextTestkit = BFilProject("scalext-testkit", file("scalext-testkit"))
+    .settings(libraryDependencies ++= Dependencies.none)
+    .dependsOn(scalext)
+
+  lazy val scalextTests = BFilProject("scalext-tests", file("scalext-tests"))
+    .settings(libraryDependencies ++= Dependencies.tests(scalaVersion.value))
+    .dependsOn(scalext, scalextTestkit)
+    .settings(Publish.noPublish: _*)
 }
 
 object Dependencies {
-  def all(scalaVersion: String) = Seq(
-    "com.typesafe.akka" %% "akka-actor" % "2.3.9",
-    "org.specs2" %% "specs2-core" % "2.4.17" % "test",
-    "org.specs2" %% "specs2-mock" % "2.4.17" % "test",
-    "org.mockito" % "mockito-all" % "1.10.19" % "test",
-    "org.hamcrest" % "hamcrest-all" % "1.3" % "test")
+  def core(scalaVersion: String) = Seq(
+    "com.typesafe.akka" %% "akka-actor" % "2.3.9")
+
+  def tests(scalaVersion: String) = Seq(
+    "org.specs2" %% "specs2-core" % "2.4.17",
+    "org.specs2" %% "specs2-mock" % "2.4.17",
+    "org.mockito" % "mockito-all" % "1.10.19",
+    "org.hamcrest" % "hamcrest-all" % "1.3")
+
+  val none = Seq.empty
 }
