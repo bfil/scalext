ce77d8abbe9f5f886c3b163e303976be439516e6
Bruno Filippone <bruno.filippone@b-fil.com>
2015-03-22 10:17:10 +0000

    Bumped version to 0.3.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.bfil.sbt._
 
 object ProjectBuild extends BFilBuild {
 
-  val buildVersion = "0.2.0"
+  val buildVersion = "0.3.0-SNAPSHOT"
 
   lazy val root = BFilRootProject("root", file("."))
     .aggregate(scalext, scalextTestkit, scalextTests)
